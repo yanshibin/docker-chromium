40b76cbeb78418eddd1c885a266392c5d13b3fa4
Vincent <8521118@gmail.com>
2024-04-04 21:57:12 +0800

    Update check-snd.sh

--- rootfs/etc/cont-init.d/check-snd.sh
+++ rootfs/etc/cont-init.d/check-snd.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-set -e # Exit immediately if a command exits with a non-zero status.
-set -u # Treat unset variables as an error.
+set -e # 立即退出，如果命令返回非零状态。
+set -u # 将未设置的变量视为错误。
 
 log() {
     echo "[cont-init.d] $(basename $0): $*"
@@ -14,9 +14,14 @@ if [ ! -d "$SND_DEV" ]; then
     exit 0
 fi
 
-# Save the associated group.
+# 检测声音设备的关联组。
 SND_GRP="$(find "$SND_DEV" -maxdepth 1 -not -type d -exec stat -c "%g" {} \; | sort -u | tail -n1)"
 log "sound device group $SND_GRP."
+# 确保目标目录存在。
+if [ ! -d /var/run/s6/container_environment ]; then
+    mkdir -p /var/run/s6/container_environment
+fi
+# 向 SUP_GROUP_IDS 添加声音组 ID。
 if [ -f /var/run/s6/container_environment/SUP_GROUP_IDS ]; then
     echo -n "," >> /var/run/s6/container_environment/SUP_GROUP_IDS
 fi
